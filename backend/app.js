df76ff244ace6b3afa3b873349b317b6a958f043
Bino Nguyen <binonguyen1090@gmail.com>
2023-11-22 17:57:34 -0600

    initial set up files

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv'
+
+dotenv.config({ path: "backend/config/config.env" })
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started on PORT: 3000`);
+});
