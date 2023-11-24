318e7fb5036203ea44abee8eed9fd1b914d3c8f6
Bino Nguyen <binonguyen1090@gmail.com>
2023-11-24 10:22:17 -0600

    set up 1st route/controller

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,13 @@ const app = express();
 import dotenv from 'dotenv'
 
 dotenv.config({ path: "backend/config/config.env" })
-console.log(process.env.NODE_ENV)
+
+
+// import routes
+import productRoutes from './routes/products.js'
+
+app.use('/api/v1', productRoutes)
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
---
+++ backend/routes/products.js
@@ -0,0 +1,11 @@
+
+
+import express from 'express'
+import { getProducts } from '../controllers/productController.js';
+
+const router = express.Router();
+
+
+router.route("/products").get(getProducts);
+
+export default router;
