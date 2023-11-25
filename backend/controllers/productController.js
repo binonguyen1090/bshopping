d65418f46abe7c67e07fd6b9af847ec03b676ece
Bino Nguyen <binonguyen1090@gmail.com>
2023-11-25 10:24:08 -0600

    get all products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,8 +3,10 @@
 import Product from "../models/product.js"
 
 export const getProducts = async (req,res) => {
+
+    const products = await Product.find()
     res.status(200).json({
-        message: "All Products"
+        products,
     })
 }
 
