f6c3ad4a2a9199522512469cd1fa71297a146726
Bino Nguyen <binonguyen1090@gmail.com>
2023-11-27 19:31:56 -0600

    update db string for seeder

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -9,7 +9,8 @@ const seedProducts = async () => {
 
 
     try {
-        await mongoose.connect("mongodb://localhost:27017/bshopping")
+
+        await mongoose.connect("mongodb://127.0.0.1:27017/bshopping")
         await Product.deleteMany()
         console.log('Products are deleted')
 
