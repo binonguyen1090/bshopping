b59b9991409d800bf863e69784ed5db359269687
Bino Nguyen <binonguyen1090@gmail.com>
2023-11-30 21:26:07 -0600

    get all orders / update process orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,7 @@
 
 
 import Order from "../models/order.js"
+import Product from "../models/product.js"
 import ErrorHandler from "../utils/errorHandler.js"
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js"
 import APIFilters from "../utils/apiFilter.js"
@@ -52,16 +53,57 @@ export const getOrderDetail = catchAsyncErrors(async (req,res,next) => {
 
 export const myOrder = catchAsyncErrors(async (req,res,next) => {
     
-    console.log(req?.user?._id);
     const order = await Order.find({user: req?.user?._id})
     if(!order){
         return next(new ErrorHandler("Order not found", 404))
-        // return  res.status(404).json({
-        //     error: "Product not found",
-        // })
     }
     res.status(200).json({
         order
     })
 })
 
+
+export const getAllOrder = catchAsyncErrors(async (req,res,next) => {
+    
+    const orders = await Order.find()
+    if(!orders){
+        return next(new ErrorHandler("Orders not found", 404))
+
+    }
+    res.status(200).json({
+        orders
+    })
+})
+
+export const updateOrder = catchAsyncErrors(async (req,res,next) => {
+    
+    const order = await Order.findById(req.params.id)
+    if(!order){
+        return next(new ErrorHandler("Orders not found", 404))
+
+    }
+
+
+    if(order?.orderStatus === "Delivered"){
+        return next(new ErrorHandler("Already delivered", 404))
+    }
+
+    //update product stock
+    
+    order.orderItems.forEach(async(item)=>{
+        const product = await Product.findById(item.product.toString())
+        
+        if(!product){
+            return next(new ErrorHandler("Product not found", 404))
+        }
+        product.stock = product.stock - item.quantity
+        await product.save({validateBeforeSave: false})
+    })
+
+    order.orderStatus = req.body.status
+    order.deliveredAt = Date.now()
+    await order.save()
+    res.status(200).json({
+        success: true
+    })
+})
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,14 +2,17 @@
 
 import express from 'express'
 import { isAuthenticatedUser, authorizeRoles } from '../middlewares/auth.js';
-import { newOrder,getOrderDetail,myOrder } from '../controllers/orderController.js';
+import { newOrder,getOrderDetail,myOrder,getAllOrder,updateOrder} from '../controllers/orderController.js';
 
 const router = express.Router();
 
 
 router.route("/orders/new").post(isAuthenticatedUser,newOrder);
 router.route("/orders/:id").get(isAuthenticatedUser,getOrderDetail);
+
 router.route("/user/orders").get(isAuthenticatedUser,myOrder);
 
+router.route("/admin/orders").get(isAuthenticatedUser,authorizeRoles('admin'),getAllOrder);
+router.route("/admin/orders/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder);
 
 export default router;
