9587ffc5535bf41d5e32831ffc50a4fa650f1480
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-03 19:21:11 -0600

    set up 1st path  react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,21 @@ import Header from "./components/layout/Header";
 import Home from "./components/Home";
 import './App.css'
 
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <Home />
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home /> } />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
