ce6119a394b9ccd18961f305606f15ab5c05dfdd
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-03 18:50:05 -0600

    add Header Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>B-Shopping</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
