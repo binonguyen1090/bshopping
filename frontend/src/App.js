c826698d040c8314d677d3d28cf590426b0b79f4
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-03 18:57:44 -0600

    set up Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+
+import Home from "./components/Home";
 import './App.css'
 
 
@@ -7,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Home />
+      </div>
       <Footer />
     </div>
   );
