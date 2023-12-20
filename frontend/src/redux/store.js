4a0e9e27e30cd5d7b6f0a6d2f5a9f0cbe12c6cbe
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-19 21:11:48 -0600

    Load Loggin User in State

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,24 @@
 import { MiddlewareArray, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { productApi } from "./api/productsApi";
 import { authApi } from "./api/authApi";
+import { userApi } from "./api/userApi";
+import userReducer from "./features/userSlice";
 
 
 export const store = configureStore({
     reducer: {
+        auth: userReducer,
         [productApi.reducerPath] : productApi.reducer,
         [authApi.reducerPath]: authApi.reducer,
+        [userApi.reducerPath]: userApi.reducer,
+
     },
     middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware().concat(
             [productApi.middleware],
-            [authApi.middleware]
+            [authApi.middleware],
+            [userApi.middleware]
+
 
         )
 })
