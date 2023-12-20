68b3eec2d1e30cb7cd4ababe8fc32514dba38cfd
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-19 21:18:41 -0600

    display name on Header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Search from './Search'
 import { useGetMeQuery } from '../../redux/api/userApi'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
-  const {data} = useGetMeQuery()
+  const {isLoading} = useGetMeQuery()
+  const {user} = useSelector((state)=>state.auth)
 
-  
+
   return (
     <nav className="navbar row">
       <div className="col-12 col-md-3 ps-5">
@@ -24,36 +27,40 @@ const Header = () => {
           <span id="cart" className="ms-3"> Cart </span>
           <span className="ms-1" id="cart_count">0</span>
         </a>
+        {user ? (
+          <div className="ms-4 dropdown">
+            <button
+              className="btn dropdown-toggle text-white"
+              type="button"
+              id="dropDownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <figure className="avatar avatar-nav">
+                <img
+                  src={user?.avatar ? user?.avatar?.url :"../images/default_avatar.jpg"}
+                  alt="User Avatar"
+                  className="rounded-circle"
+                />
+              </figure>
+              <span>{user.name}</span>
+            </button>
+            <div className="dropdown-menu w-100" aria-labelledby="dropDownMenuButton">
+              <a className="dropdown-item" href="/admin/dashboard"> Dashboard </a>
 
-        <div className="ms-4 dropdown">
-          <button
-            className="btn dropdown-toggle text-white"
-            type="button"
-            id="dropDownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <figure className="avatar avatar-nav">
-              <img
-                src="../images/default_avatar.jpg"
-                alt="User Avatar"
-                className="rounded-circle"
-              />
-            </figure>
-            <span>User</span>
-          </button>
-          <div className="dropdown-menu w-100" aria-labelledby="dropDownMenuButton">
-            <a className="dropdown-item" href="/admin/dashboard"> Dashboard </a>
+              <a className="dropdown-item" href="/me/orders"> Orders </a>
 
-            <a className="dropdown-item" href="/me/orders"> Orders </a>
+              <a className="dropdown-item" href="/me/profile"> Profile </a>
 
-            <a className="dropdown-item" href="/me/profile"> Profile </a>
-
-            <a className="dropdown-item text-danger" href="/"> Logout </a>
+              <a className="dropdown-item text-danger" href="/"> Logout </a>
+            </div>
           </div>
-        </div>
+        ): (
+          !isLoading && (
+            <Link to="/login" className="btn ms-4" id="login_btn"> Login </Link>
+          )
+        )}
 
-        <a href="/login" className="btn ms-4" id="login_btn"> Login </a>
       </div>
     </nav>
 
