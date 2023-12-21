55a208e11452a7dd64eda42f3941488ce5ad55a8
Bino Nguyen <binonguyen1090@gmail.com>
2023-12-20 21:38:06 -0600

    Forgot and Reset Password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ import UpdateProfile from "./components/user/UpdateProfile";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UploadAvatar from "./components/user/UploadAvatar";
 import UpdatePassword from "./components/user/UpdatePassword";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import ResetPassword from "./components/auth/ResetPassword";
 function App() {
   return (
     <Router>
@@ -25,13 +27,17 @@ function App() {
             <Route path="/product/:id" element={<ProductDetails /> } />
             <Route path="/login" element={<Login /> } />
             <Route path="/register" element={<Register /> } />
+            <Route path="/password/forgot" element={<ForgotPassword /> } />
+            <Route path="/password/reset/:token" element={<ResetPassword /> } />
+
+
             <Route path="/me/profile" element={
               <ProtectedRoute><Profile /></ProtectedRoute>
              } />
             <Route path="/me/update_profile" element={<ProtectedRoute><UpdateProfile /></ProtectedRoute> } />
             <Route path="/me/upload_avatar" element={<ProtectedRoute><UploadAvatar /></ProtectedRoute> } />
             <Route path="/me/update_password" element={<ProtectedRoute><UpdatePassword /></ProtectedRoute> } />
-
+          
           </Routes>
         </div>
         <Footer />
